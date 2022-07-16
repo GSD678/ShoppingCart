ed7da623ff979d15bdc683ac80a243bdf56d4d55
Karim <kimo_678@live.com>
2022-07-16 14:24:59 -0400

    added routing and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import { Container } from "react-bootstrap";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <main>
-        <Container>
-          <HomeScreen />
+      <Router>
+        <Header />
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" exact element={<HomeScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
           </Container>
-      </main>
-      <Footer />
-    </>
+        </main>
+        <Footer />
+      </Router>
+      </>
   );
 }
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import { Container, Navbar,Nav } from "react-bootstrap";
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+
+
 const Header = () => {
   return (
-      <header>
-          <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-              <Container>
-                  <Navbar.Brand href="/">My Shopping Cart</Navbar.Brand>
-                  <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                  <NavbarCollapse id="basic-navbar-nav">
-                      <Nav className="ms-auto">
-                          <Nav.Link href="/cart">
-                              <i className="fas fa-shopping-cart"></i>
-                               Cart
-                          </Nav.Link>
-                          <Nav.Link href="/login">
-                              <i className="fas fa-user">
-                              </i>                      
-                               Sign in
-                            </Nav.Link>
-                      </Nav>
-                  </NavbarCollapse>
-              </Container>
-          </Navbar>
-      </header>
-      
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>MyShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link ><i className='fas fa-user'></i> Sign In</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
